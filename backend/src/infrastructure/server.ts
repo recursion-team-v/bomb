7411fe5e5377ac0c7c8b02363f56a1fdebcbb76b
gin <10017674+lirlia@users.noreply.github.com>
2022-12-19 22:29:00 +0900

    #12 add socket io chat

--- backend/src/infrastructure/server.ts
+++ backend/src/infrastructure/server.ts
@@ -1,13 +1,29 @@
-import * as http from 'http';
-
 import { port } from '../config/config';
 
 export const listen = () => {
-  const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World Node.js x TypeScript');
-  });
-  server.listen(port);
+  const app = require('express')();
+  const http = require('http').Server(app);
+  const io = require('socket.io')(http);
 
-  console.log(`Server running at http://localhost:${port}/`);
+  app.get('/', (req: any, res: { sendFile: (arg0: string) => void }) => {
+    res.sendFile(__dirname + '/index.html');
+  });
+
+  io.on('connection', (socket: { on: (arg0: string, arg1: (msg: any) => void) => void }) => {
+    socket.on('chat message', (msg: any) => {
+      io.emit('chat message', msg);
+    });
+  });
+
+  http.listen(port, () => {
+    console.log(`Socket.IO server running at http://localhost:${port}/`);
+  });
+
+  // const server = http.createServer((req, res) => {
+  //   res.writeHead(200, { 'Content-Type': 'text/plain' });
+  //   res.end('Hello World Node.js x TypeScript');
+  // });
+  // server.listen(port);
+
+  // console.log(`Server running at http://localhost:${port}/`);
 };
