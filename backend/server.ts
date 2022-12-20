bd0445af164fdc9a252d5bdbeee85b8295d698dd
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2022-12-20 02:50:13 -0600

    feat: add prettier git actions

--- backend/server.ts
+++ backend/server.ts
@@ -1,21 +1,26 @@
 import express from 'express';
 import http from 'http';
+import path from 'path';
 import { Server } from 'socket.io';
 
 const app: express.Express = express();
 const server: http.Server = http.createServer(app);
 const io: Server = new Server(server);
 
-const players: { [key: number]: Player } = {};
 
-app.use(express.static(__dirname + "/public"));
 
-app.get("/", function (req: express.Request<any>, res: express.Response<any>) {
-  res.sendFile(__dirname + "/index.html");
+
+
+const players: Record<number, Player> = {};
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function (req: express.Request<any>, res: express.Response<any>) {
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-io.on("connection", function (socket: any) {
-  console.log("a user connected");
+io.on('connection', function (socket: any) {
+  console.log('a user connected');
 
   // create a new player and add it to our players object
   players[socket.id] = {
@@ -26,31 +31,33 @@ io.on("connection", function (socket: any) {
   };
 
   // send the players object to the new player
-  socket.emit("currentPlayers", players);
+  socket.emit('currentPlayers', players);
 
   // update all other players of the new player
-  socket.broadcast.emit("newPlayer", players[socket.id]);
+  socket.broadcast.emit('newPlayer', players[socket.id]);
 
   // when a player disconnects, remove them from our players object
-  socket.on("disconnect", function () {
-    console.log("user disconnected");
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
     // remove this player from our players object
+    // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
     delete players[socket.id];
     // emit a message to all players to remove this player
-    io.emit("disconnected", socket.id);
+    io.emit('disconnected', socket.id);
   });
 
   // when a player moves, update the player data
-  socket.on("playerMovement", function (movementData: Player) {
+  socket.on('playerMovement', function (movementData: Player) {
     players[socket.id].x = movementData.x;
     players[socket.id].y = movementData.y;
     players[socket.id].rotation = movementData.rotation;
     // emit a message to all players about the player that moved
-    socket.broadcast.emit("playerMoved", players[socket.id]);
+    socket.broadcast.emit('playerMoved', players[socket.id]);
   });
 });
 
-const port = process.env.PORT || 8081;
+// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+const port = process.env.PORT ?? 8081;
 server.listen(port, function () {
   console.log(`Listening on ${port} `);
 });
