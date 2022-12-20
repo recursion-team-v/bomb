e37bf46220f19dabacb404cd8105642dac5c7bca
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2022-12-20 02:25:23 -0600

    refactor: app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
