901c21603ad75f27b50a9c1c7d7113600a811d5a
satojin <renzuoteng142@gmail.com>
2022-12-20 18:52:55 +0900

    #9 chore:install react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App() {
+export const App:React.FC = ()=> {
   const [count, setCount] = useState(0);
 
   return (
@@ -27,4 +27,4 @@ function App() {
   );
 }
 
-export default App;
+
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RecoilRoot } from "recoil";
-import App from "./App";
 import "@unocss/reset/normalize.css";
 import "./index.css";
+import { RouterConfig } from "./RouterConfig";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <RecoilRoot>
-    <App />
+    <RouterConfig />
     </RecoilRoot>
   </React.StrictMode>
 );
