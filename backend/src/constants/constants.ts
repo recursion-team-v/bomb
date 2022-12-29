1238f20ec864a04ebdf8d18863e1f52c5cae0fe4
gin <10017674+lirlia@users.noreply.github.com>
2022-12-30 03:02:22 +0900

    add comment

--- backend/src/constants/constants.ts
+++ backend/src/constants/constants.ts
@@ -1,8 +1,9 @@
 export const FPS = 120; // 1 秒間のフレーム数
 export const FRAME_RATE = 1000 / FPS; // 1 frame にかかる時間(ms)
 
-// ゲームルーム参加時に使用するキー
+// Dockerfile の中と、デプロイ時にポートを指定しているので、ここの設定は開発時にしか利用されません。
 export const SERVER_LISTEN_PORT = 2567;
+// ゲームルーム参加時に使用するキー
 export const GAME_ROOM_KEY = 'game';
 
 export const NOTIFICATION_TYPE = {
