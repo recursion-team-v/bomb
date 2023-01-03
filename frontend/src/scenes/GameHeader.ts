8cd1bf5443a7bd183a01a5b6f1574f065c2078f4
gin <10017674+lirlia@users.noreply.github.com>
2023-01-04 02:44:32 +0900

    add color util

--- frontend/src/scenes/GameHeader.ts
+++ frontend/src/scenes/GameHeader.ts
@@ -3,12 +3,11 @@ import Phaser from 'phaser';
 import * as Constants from '../../../backend/src/constants/constants';
 import * as Config from '../config/config';
 import convertSecondsToMMSS from '../utils/timer';
+import ToString from '../utils/color';
 
 export default class GameHeader extends Phaser.Scene {
   private readonly width: number;
   private readonly height: number;
-  private readonly headerColorCode: string;
-  private readonly headerTimerTextColorCode: string;
 
   private timerText!: Phaser.GameObjects.Text;
   private remainTime: number = 0;
@@ -18,8 +17,6 @@ export default class GameHeader extends Phaser.Scene {
 
     this.height = Constants.HEADER_HEIGHT;
     this.width = Constants.HEADER_WIDTH;
-    this.headerColorCode = Constants.HEADER_COLOR_CODE.toString(16);
-    this.headerTimerTextColorCode = Constants.HEADER_TIMER_TEXT_COLOR_CODE.toString(16);
   }
 
   init() {
@@ -29,11 +26,11 @@ export default class GameHeader extends Phaser.Scene {
 
     this.timerText = this.add
       .text(0, 0, '', { fontSize: `${fontSize}px` })
-      .setColor(this.headerTimerTextColorCode)
+      .setColor(ToString(Constants.HEADER_TIMER_TEXT_COLOR_CODE))
       .setPadding(10, paddingHeight, 10, paddingHeight);
 
     this.cameras.main.setSize(this.width, this.height);
-    this.cameras.main.setBackgroundColor(this.headerColorCode);
+    this.cameras.main.setBackgroundColor(ToString(Constants.HEADER_COLOR_CODE));
   }
 
   updateTimerText(timeLimit: number) {
