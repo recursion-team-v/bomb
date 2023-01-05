9d823795e229d58f737275e3ccc89e7a3420b6c3
gin <10017674+lirlia@users.noreply.github.com>
2023-01-06 03:18:01 +0900

    delete の実装

--- backend/src/game_engine/services/blastService.ts
+++ backend/src/game_engine/services/blastService.ts
@@ -1,7 +1,11 @@
+import Matter from 'matter-js';
+
 import GameEngine from '../../rooms/GameEngine';
 
 export default class BlastService {
   private readonly gameEngine: GameEngine;
+  // eslint-disable-next-line @typescript-eslint/prefer-readonly
+  private blastComposite: Matter.Composite;
 
   constructor(gameEngine: GameEngine) {
     this.gameEngine = gameEngine;
@@ -11,7 +15,10 @@ export default class BlastService {
   add() {}
 
   // 爆風を matter から削除する
-  private delete() {}
+  private delete() {
+    if (this.blastComposite === undefined) return;
+    Matter.Composite.remove(this.gameEngine.world, this.blastComposite);
+  }
 
   // 爆風の範囲を計算する
   private calcBlastRange() {}
