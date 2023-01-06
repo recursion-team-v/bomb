83d07d2b5b4801bd9f1add506b7095ce634ee8bd
Hiroto Aoyama <aoyamahiroto0222@gmail.com>
2023-01-06 13:13:09 -0600

    #139 remove addItems

--- frontend/src/scenes/Game.ts
+++ frontend/src/scenes/Game.ts
@@ -55,9 +55,6 @@ export default class Game extends Phaser.Scene {
     // Colyseus のイベントを追加
     this.initNetworkEvents();
 
-    // add items
-    this.addItems();
-
     // TODO: Preloader（Lobby）で読み込んで Game Scene に渡す
     this.room.onStateChange.once((state) => {
       // GameRoomState の blockArr が初期化されたら block（破壊）を描画
@@ -190,39 +187,6 @@ export default class Game extends Phaser.Scene {
     this.otherPlayers.forEach((otherPlayer: OtherPlayer) => {
       otherPlayer.update();
     });
-  }
-
-  private addItems() {
-    this.add.item(
-      64 * Phaser.Math.Between(1, 13) + 32,
-      64 * Phaser.Math.Between(1, 11) + Constants.HEADER_HEIGHT + 32,
-      Constants.ITEM_TYPE.BOMB_STRENGTH
-    );
-    this.add.item(
-      64 * Phaser.Math.Between(1, 13) + 32,
-      64 * Phaser.Math.Between(1, 11) + Constants.HEADER_HEIGHT + 32,
-      Constants.ITEM_TYPE.BOMB_STRENGTH
-    );
-    this.add.item(
-      64 * Phaser.Math.Between(1, 13) + 32,
-      64 * Phaser.Math.Between(1, 11) + Constants.HEADER_HEIGHT + 32,
-      Constants.ITEM_TYPE.BOMB_STRENGTH
-    );
-
-    const bombPossessionUpCount = 10;
-    for (let i = 0; i < bombPossessionUpCount; i++) {
-      this.add.item(
-        64 * Phaser.Math.Between(1, 13) + 32,
-        64 * Phaser.Math.Between(1, 11) + Constants.HEADER_HEIGHT + 32,
-        Constants.ITEM_TYPE.BOMB_POSSESSION_UP
-      );
-    }
-
-    // this.add.item(
-    //   64 * Phaser.Math.Between(1, 13) + 32,
-    //   64 * Phaser.Math.Between(1, 11) + Constants.HEADER_HEIGHT + 32,
-    //   ItemTypes.PLAYER_SPEED
-    // );
   }
 
   public getCurrentPlayer(): MyPlayer {
