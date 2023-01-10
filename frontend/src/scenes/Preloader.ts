6c4d483182a4a04bc235e772cd4b5858bd4b77ee
gin <10017674+lirlia@users.noreply.github.com>
2023-01-10 15:26:55 +0900

    音をON OFF するボタンの追加

--- frontend/src/scenes/Preloader.ts
+++ frontend/src/scenes/Preloader.ts
@@ -1,12 +1,12 @@
 import Phaser from 'phaser';
 
 import * as Constants from '../../../backend/src/constants/constants';
-import * as Config from '../config/config';
-import isMobile from '../utils/mobile';
-import Network from '../services/Network';
-import { createPlayerAnims } from '../anims/PlayerAnims';
 import { createBombAnims } from '../anims/BombAnims';
 import { createExplodeAnims } from '../anims/explodeAnims';
+import { createPlayerAnims } from '../anims/PlayerAnims';
+import * as Config from '../config/config';
+import Network from '../services/Network';
+import isMobile from '../utils/mobile';
 
 export default class Preloader extends Phaser.Scene {
   private preloadComplete = false;
@@ -65,6 +65,10 @@ export default class Preloader extends Phaser.Scene {
     this.load.image(Constants.ITEM_TYPE.BOMB_POSSESSION_UP, 'assets/items/item_bomb_up.png');
     this.load.image(Constants.ITEM_TYPE.BOMB_STRENGTH, 'assets/items/item_bomb_strength.png');
     this.load.image(Constants.ITEM_TYPE.PLAYER_SPEED, 'assets/items/item_player_speed.png');
+
+    // icon
+    this.load.image(Config.ASSET_KEY_VOLUME_ON, 'assets/icons/volume_on.png');
+    this.load.image(Config.ASSET_KEY_VOLUME_OFF, 'assets/icons/volume_off.png');
 
     if (isMobile()) {
       this.load.image(Constants.JOYSTICK_BASE_KEY, 'assets/joystick-base.png');
---
+++ frontend/src/utils/sound.ts
@@ -0,0 +1,10 @@
+import { phaserGlobalGameObject } from '../PhaserGame';
+
+export function isMute(): boolean {
+  return phaserGlobalGameObject().sound.mute;
+}
+
+// on / off を切り替える
+export function toggle() {
+  phaserGlobalGameObject().sound.mute = !isMute();
+}
