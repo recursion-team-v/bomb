fd96b047a02be75d749ac222ca7a1eaae5918d6c
satojin219 <satojin219@users.noreply.github.com>
2023-01-16 15:11:54 +0000

    refactor: format code

--- frontend/src/utils/usage.ts
+++ frontend/src/utils/usage.ts
@@ -18,12 +18,11 @@ export const createUsageDialog = function (scene: Phaser.Scene, config: any) {
       space: { left: -5, right: -5, top: -5, bottom: -5 },
 
       background: scene.rexUI.add.roundRectangle(x, y, 2, 2, 10),
-      
+
       // main: this.add.text(0, 0, 'Item', { fontSize: '20px', color: 'white' }),
       main: scene.rexUI.add.label({
         icon: scene.rexUI.add.roundRectangle(x, y, 10, 10, 5),
         text: scene.add.text(0, 0, 'Item'),
-      
       }),
     })
     .layout();
